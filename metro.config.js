6aa6311a9a6a3a9131b525a7ac515ef09168f716
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2022-02-09 13:27:31 -0500

    test

--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,17 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
  * @format
  */
 
-'use strict';
-
-const getPolyfills = require('./rn-get-polyfills');
-
-/**
- * This cli config is needed for development purposes, e.g. for running
- * integration tests during local development or on CI services.
- */
 module.exports = {
-  resolver: {
-    // $FlowFixMe[signature-verification-failure] Can't infer RegExp type.
-    blockList: /buck-out/,
-    extraNodeModules: {
-      'react-native': __dirname,
-    },
-  },
-  serializer: {
-    getPolyfills,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
 };
