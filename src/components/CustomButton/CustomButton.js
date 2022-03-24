989377ef4b24cc51dbbd5911b8d7ff344e3a4d49
Juan42145 <juancamilo.arias@hotmail.com>
2022-03-24 15:14:07 -0500

    Added button pressed style

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,53 +2,54 @@
 import React from 'react';
 import { View, Text, StyleSheet, Pressable} from 'react-native';
 
-const CustomButton = ({text, onPress, type}) => {
+const CustomButton = ({text, onPress, type = "PRIMARY"}) => {
 
   return (
-    <Pressable onPress={onPress} style={[styles.container, styles[`container_${type}`]]}>
-      <Text style={styles.text, styles[`text_${type}`]}>{text}</Text>
+    <Pressable onPress={onPress} style={({pressed}) => pressed ? [styles.container, styles[`pressed_${type}`]] : [styles.container, styles[`container_${type}`]]}>
+      <Text style={[styles.text, styles[`text_${type}`]]}>{text}</Text>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  container_PRIMARY:{
-    backgroundColor: '#FFFFFF',
+  container:{
     width: 150,
     padding: 8.5,
     marginVertical: 5,
     alignItems: 'center',
     borderRadius: 25,
   },
+  container_PRIMARY:{
+    backgroundColor: '#EDE9E9',
+  },
   container_SECONDARY:{
     backgroundColor: '#736468',
-    width: 150,
-    padding: 8.5,
-    marginVertical: 5,
-    alignItems: 'center',
-    borderRadius: 25,
   },
   container_TERTIARY:{
     backgroundColor: 'rgba(52, 52, 52, 0.0)',
     width: 300,
-    padding: 8.5,
-    marginVertical: 5,
-    alignItems: 'center',
-    borderRadius: 25,
   },
-   text_PRIMARY:{
+  pressed_PRIMARY:{
+    backgroundColor: '#D5D2D2',
+  },
+  pressed_SECONDARY:{
+    backgroundColor: '#675A5E',
+  },
+  pressed_TERTIARY:{
+    width: 300,
+  },
+  text:{
     fontWeight: 'bold',
-    color: '#736468',
     fontSize: 24,
+  },
+  text_PRIMARY:{
+    color: '#736468',
   },
   text_SECONDARY: {
-    fontWeight: 'bold',
-    color: 'white',
-    fontSize: 24,
+    color: '#EDE9E9',
   },
   text_TERTIARY: {
-    fontWeight: 'bold',
-    color: '#FFFFFF',
+    color: '#EDE9E9',
     fontSize: 18,
   },
 })
