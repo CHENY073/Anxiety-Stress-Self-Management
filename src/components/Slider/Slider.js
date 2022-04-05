0ab5169b1bdc483e7dfb634c7e517ddca4fbfad5
Juan42145 <juancamilo.arias@hotmail.com>
2022-04-05 16:03:29 -0500

    Add slider image component

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,11 +1,17 @@
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
 import {View, StyleSheet, useWindowDimensions, Animated} from 'react-native';
-import Svg, {Circle, G, Path, Polygon} from 'react-native-svg';
+import Svg, {Circle, G, Path, Polygon, Image} from 'react-native-svg';
 
 const Slider = ({value, setValue, showImages}) => {
   const window = useWindowDimensions();
   const size = window.width-40;
+
+  const icon1 = '../../../assets/images/Logo.png';
+  const icon2 = '../../../assets/images/Logo.png';
+  const icon3 = '../../../assets/images/Logo.png';
+  const icon4 = '../../../assets/images/Logo.png';
+  const icon5 = '../../../assets/images/Logo.png';
 
   const AnimatedG = Animated.createAnimatedComponent(G);
   const [color, setColor] = useState(new Animated.Value(0));
@@ -66,18 +72,23 @@ const Slider = ({value, setValue, showImages}) => {
         <G scale={size/100} y="20" origin="0, -20">
           <G onPress={() => handle(1)} rotation="0" origin="50, 30">
             <Path fill="#F07575" d="m25,30h-25c0,-11,3.5,-21.2,9.5,-29.4l20.3,14.7c-3,4.1,-4.8,9.2,-4.8,14.7z"/>
+            {showImages? <Image href={require(icon1)} rotation="-0" origin="14, 18" x="4" y="8" width="20" height="20"/> : <></>}
           </G>
           <G onPress={() => handle(2)} rotation="36" origin="50, 30">
             <Path fill="#F0B275" d="m25,30h-25c0,-11,3.5,-21.2,9.5,-29.4l20.3,14.7c-3,4.1,-4.8,9.2,-4.8,14.7z"/>
+            {showImages? <Image href={require(icon2)} rotation="-36" origin="14, 18" x="4" y="8" width="20" height="20"/> : <></>}
           </G>
           <G onPress={() => handle(3)} rotation="72" origin="50, 30">
             <Path fill="#F0F075" d="m25,30h-25c0,-11,3.5,-21.2,9.5,-29.4l20.3,14.7c-3,4.1,-4.8,9.2,-4.8,14.7z"/>
+            {showImages? <Image href={require(icon3)} rotation="-72" origin="14, 18" x="4" y="8" width="20" height="20"/> : <></>}
           </G>
           <G onPress={() => handle(4)} rotation="108" origin="50, 30">
             <Path fill="#B2F075" d="m25,30h-25c0,-11,3.5,-21.2,9.5,-29.4l20.3,14.7c-3,4.1,-4.8,9.2,-4.8,14.7z"/>
+            {showImages? <Image href={require(icon4)} rotation="-108" origin="14, 18" x="4" y="8" width="20" height="20"/> : <></>}
           </G>
           <G onPress={() => handle(5)} rotation="144" origin="50, 30">
             <Path fill="#75F075" d="m25,30h-25c0,-11,3.5,-21.2,9.5,-29.4l20.3,14.7c-3,4.1,-4.8,9.2,-4.8,14.7z"/>
+            {showImages? <Image href={require(icon5)} rotation="-144" origin="14, 18" x="4" y="8" width="20" height="20"/> : <></>}
           </G>
           <AnimatedG style={rotationStyle}>
             <Polygon fill="#6D828F" points="50,35 50,25 20,30"/>
