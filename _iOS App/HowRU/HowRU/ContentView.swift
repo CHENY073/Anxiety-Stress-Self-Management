525372101d9061fac5c1a7cb9b62f14a674983b9
Raul Gutierrez <raulgniubo@gmail.com>
2022-05-27 21:47:25 -0400

    iOS App xcode project created

---
+++ _iOS App/HowRU/HowRU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowRU
+//
+//  Created by Raul Gutierrez Niubo on 5/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ _iOS App/HowRU/HowRU/HowRUApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowRUApp.swift
+//  HowRU
+//
+//  Created by Raul Gutierrez Niubo on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HowRUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
