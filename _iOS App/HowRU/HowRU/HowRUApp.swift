5db729760879d97098b2c7cf773d029ec1e5d4cb
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2022-05-31 16:10:29 -0400

    Revert "Added Firebase to iOS App"
    This reverts commit ff4a0d72582e86550bfa56b06204a9b0c793b81f.

--- _iOS App/HowRU/HowRU/HowRUApp.swift
+++ _iOS App/HowRU/HowRU/HowRUApp.swift
@@ -6,15 +6,9 @@
 //
 
 import SwiftUI
-import Firebase
 
 @main
 struct HowRUApp: App {
-    
-    init() {
-        FirebaseApp.configure()
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
