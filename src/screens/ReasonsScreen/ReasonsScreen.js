c9a7fb3f73ff397db57ece1b1ddda650944aee7b
JorgeAgue <98124296+JorgeAgue@users.noreply.github.com>
2022-06-23 15:55:27 -0400

    Added Awareness Screen.
    New screen is located in the series of Daily Stressors screens.

--- src/screens/ReasonsScreen/ReasonsScreen.js
+++ src/screens/ReasonsScreen/ReasonsScreen.js
@@ -23,7 +23,7 @@ const ReasonsScreen = ({ route, navigation }) => {
 
   const handlePress = () => {
     if(reasons.length < 1) Alert.alert('Please pick a reason');
-    else navigation.navigate('FoodFT');
+    else navigation.navigate('Awareness');
   };
 
   return (
