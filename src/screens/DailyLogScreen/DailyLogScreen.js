55dbead7da66076dfc88614d5d0489f3d3aae50e
JorgeAgue <98124296+JorgeAgue@users.noreply.github.com>
2022-07-14 11:37:06 -0400

    Improvement to Daily Log Screen
    Added a text box for the activity the user did, along with a slider for the user to show how stressed they are. Also had to change the compileSDK version from 30 to 31 in the build.gradle to fix a problem I was having.

--- src/screens/DailyLogScreen/DailyLogScreen.js
+++ src/screens/DailyLogScreen/DailyLogScreen.js
@@ -1,59 +1,57 @@
 import React, {useState, useMemo, useCallback} from 'react';
 import {View, Text, ScrollView, SafeAreaView, Image, StyleSheet, useWindowDimensions, ImageBackground, Alert, Button} from 'react-native';
-import CustomSelect from '../../components/CustomSelect';
+import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import Modal from "react-native-modal";
 import {Picker} from '@react-native-picker/picker';
+import Slider from "react-native-sliders";
+
 
 import Volume from '../../../assets/images/Volume.png';
+import Logo from '../../../assets/images/Logo.png';
 
 const DailyLogScreen = ({navigation}) => {
-
-    const [activity, setActivity] = useState(0);
+    const [activity, setActivity] = useState('');
     const [triggers, setTriggers] = useState(0);
     const [signs, setSigns] = useState(0);
     const [anxietyLevel, setAnxietyLevel] = useState(0);
     const [strategies, setStrategies] = useState(0);
   
-    const activityData = ['','Home', 'Work', 'Social Setting','School'];
-    const triggersData = ['','Mind', 'Body', 'Emotion', 'Behavior'];
-    const signsData = ['','Insomnia', 'Restlessness', 'Palpitations', 'Difficulty concentrating'];
-    const anxietyLevelData = ['','1','2','3','4','5','6','7','8','9','10'];
-    const strategiesData = ['','Breathing','Positive self-talk', 'Listening to music', 'Talking to a friend'];
+    const activityData = ['','Exercise 🏃', 'Meditation 🧘', 'Reading 📖'];
+    const triggersData = ['','Home', 'School', 'Work', 'Social Life'];
+    const signsData = ['','Body', 'Mind', 'Emotion', 'Behavior'];
+
+  
+    const strategiesData = ['','Breathing','Positive self-talk', 'Listening to music', 'Talking to a friend', 'Group Support'];
   
     const handlePress = () => {
       if(!activity) Alert.alert('Please pick an activity answer');
       else if(!triggers) Alert.alert('Please pick a triggers answer');
       else if(!signs) Alert.alert('Please pick a signs answer');
-      else if(!anxietyLevel) Alert.alert('Please pick a number');
+    //  else if(!anxietyLevel) Alert.alert('Please pick a number');
       else if(!strategies) Alert.alert('Please pick a strategies answer');
       else navigation.navigate('Intention');
     };
 
     return (
         <ScrollView>
-        <SafeAreaView>
-            <View style={styles.buttonWrapper}>
+        <SafeAreaView style={[styles.root]}>
       <View style={styles.header}>
-        <View style={{width: 100}}><CustomButton text= "<" onPress={() => navigation.goBack()} type="blackBackButton"/></View>
-        <View style={{width: 100}}><Image source={Volume} style={styles.volume} resizeMode="cover" /></View>
+      <View style={{width: 150}}><CustomButton text= "<" onPress={() => navigation.goBack()} type="blackBackButton"/></View>
+        <Image source={Logo} style={styles.logo} resizeMode="cover" />
+        <View style={{width: 150}}><Image source={Volume} style={styles.volume} resizeMode="cover" /></View>
       </View>
-
+      <View style={styles.buttonWrapper}>
       <Text style = {styles.title}>
-        Daily Log
+        Daily Log 
       </Text>
-    
-      <View style = {styles.containter}>
-      <Text style = {styles.label}>What activity did you do?</Text>
-        <Picker selectedValue={triggers} onValueChange={(itemValue, itemIndex) => setTriggers(itemValue)} style = {styles.picker} numberOfLines={5}>
-          {activityData.map((item, index)=>{
-            return (
-              <Picker.Item value={index} label = {item} key={index}/>
-            );
-          })}
-        </Picker>
 
+      <View style = {styles.containter}>
+        <Text style = {styles.label}>What activity did you do?</Text>
+        <CustomInput  value={activity} setValue={setActivity} secureTextEntry={false}/>
+
+        
         <Text style = {styles.label}>What triggers did you experience?</Text>
         <Picker selectedValue={triggers} onValueChange={(itemValue, itemIndex) => setTriggers(itemValue)} style = {styles.picker} numberOfLines={5}>
           {triggersData.map((item, index)=>{
@@ -72,16 +70,18 @@ const DailyLogScreen = ({navigation}) => {
           })}
         </Picker>
 
-        <Text style = {styles.label}>Pick a number from 1 - 10 for your anxiety level</Text>
-        <Picker selectedValue={anxietyLevel} onValueChange={(itemValue, itemIndex) => setAnxietyLevel(itemValue)} style = {styles.picker} numberOfLines={5}>
-          {anxietyLevelData.map((item, index)=>{
-            return (
-              <Picker.Item value={index} label = {item} key={index}/>
-            );
-          })}
-        </Picker>
-
-        <Text style = {styles.label}>What strategies did you use?</Text>
+        <Text style = {styles.label}>How stressed are you?</Text>
+        <Slider
+          value={anxietyLevel}
+          onValueChange={(anxietyLevel, setAnxietyLevel) => setSigns(anxietyLevel)}
+          minimumTrackTintColor= '#de3c3c'
+          maximumTrackTintColor= '#5cde3c'
+          minimumValue= '0'
+          maximumValue= '10'
+          step= '2'
+        />
+        
+        <Text style = {styles.label}>What strategies can you use?</Text>
         <Picker selectedValue={strategies} onValueChange={(itemValue, itemIndex) => setStrategies(itemValue)} style = {styles.picker} numberOfLines={5}>
           {strategiesData.map((item, index)=>{
             return (
@@ -96,7 +96,7 @@ const DailyLogScreen = ({navigation}) => {
       </View>
       </View>
     </SafeAreaView>
-   </ScrollView>
+    </ScrollView>
 
   );
 };
@@ -104,13 +104,14 @@ const DailyLogScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,  
+    
   },
   header: {
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 30,
+    padding: 0,
   },
   title: {
     fontSize: 34,
@@ -120,7 +121,7 @@ const styles = StyleSheet.create({
   buttonWrapper: {
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop:10,
+    paddingTop: 0,
   },
   button:{
     flex: 1,
@@ -145,6 +146,11 @@ const styles = StyleSheet.create({
     height: 30,
     margin: 20,
     alignSelf: 'flex-end',
+  },
+  logo: {
+    maxWidth: 75,
+    maxHeight: 75,
+    marginVertical: 10,
   },
 });
 
