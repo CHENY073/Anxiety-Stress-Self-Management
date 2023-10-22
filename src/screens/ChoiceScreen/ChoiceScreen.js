6c97a2174290d0522d82225696d79a17e3538171
CHENY073 <99201199+CHENY073@users.noreply.github.com>
2023-10-22 00:22:47 -0400

    Revert "Added the monthly calendar to menu"

--- src/screens/ChoiceScreen/ChoiceScreen.js
+++ src/screens/ChoiceScreen/ChoiceScreen.js
@@ -17,42 +17,38 @@ const ChoiceScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={[styles.root]}>
       <View style={styles.header}>
-        <View style={{ width: 100 }}>
-          <CustomButton text="≡" onPress={() => navigation.navigate('Menu Screen')} type="blackBackButton" />
-        </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ marginRight: 100 }}>
+        <View style={{width: 100}}><CustomButton text= "≡" onPress={() => navigation.navigate('Menu Screen')} type="blackBackButton"/></View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+         <View style={{ marginRight: 100 }}>
             <Image source={Logo} style={styles.logo} resizeMode="cover" />
-          </View>
-        </View>
+            </View>
+            </View>
       </View>
 
-      <Text style={styles.title}>
+      <Text style = {styles.title}>
         What would you like to do?
       </Text>
-
-
-      <CustomButton text= "Immediate Exercises" onPress={() => navigation.navigate('Exercises')} type="CHOICE"/>
-      <CustomButton text="Daily Roadmap" onPress={() => navigation.navigate('Emotion')} type="CHOICE" />
-      <CustomButton text="Monthly Calendar" onPress={() => navigation.navigate('Mood Diary')} type="CHOICE" />
-
-      <View>
-        <Modal isVisible={isModalVisible} onModalHide={() => setModalVisible(false)}>
-          <View style={styles.modal}>
-            <Text style={styles.modalText}>What are Immediate Exercises?</Text>
-            <Text style={styles.modalSmallText}>Exercises to help you lower your levels of stress and anxiety. Examples include
-              breathing exercises and meditating to relaxing music. Consistently doing these exercises can help us greatly in reaching
-              a more relaxed state.</Text>
-            <Text style={styles.modalText}>What are Daily Stressors?</Text>
-            <Text style={styles.modalSmallText}>Daily stressors are day-to-day issues that cause stress and anxiety. Isolating them
-              and focusing on what they are help us understand them better and reduce them. Writing them down helps us keep track of the
-              things that cause us harm so we can work to eliminate them.</Text>
-            <Button style={styles.modalButton} title="Hide" onPress={toggleModal} />
-          </View>
-        </Modal>
+    <View style={styles.stressorView}>
+    <CustomButton text= "Immediate Exercises" onPress={() => navigation.navigate('Exercises')} type="CHOICE"/>
       </View>
+      <View >
 
-      <CustomButton style={styles.iButton} text="Learn More" onPress={toggleModal} type="INFO" />
+      <Modal isVisible={isModalVisible} onModalHide={()=> setModalVisible(false)}>
+        <View style={styles.modal}>
+          <Text style={styles.modalText}>What are Immediate Exercises?</Text>
+          <Text style={styles.modalSmallText}> Exercises to help you lower your levels of stress and anxiety. Examples include
+          breathing exercises and meditating to relaxing music. Consistently doing these exercises can help us greatly in reaching
+          a more relaxed state.</Text>
+          <Text style={styles.modalText}>What are Daily Stressors?</Text>
+          <Text style={styles.modalSmallText}> Daily stressors are day to day issues that cause stress and anxiety. Isolating them
+          and focusing on what they are help us understand them better and reduce them. Writing them down helps us keep track of the 
+          things that cause us harm so we can work to elimnate them.</Text>
+          <Button style={styles.modalButton} title="Hide" onPress={toggleModal} />
+        </View>
+      </Modal>
+    </View>
+    <CustomButton text= "Daily Stressors" onPress={() => navigation.navigate('Emotion')} type="CHOICE"/>
+    <CustomButton style={styles.iButton} text="Learn More" onPress={toggleModal} type="INFO"/>
     </SafeAreaView>
   );
 };
@@ -82,7 +78,7 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
   },
   title: {
-    fontSize: 30,
+    fontSize: 36,
     fontWeight: 'bold',
     color: '#000000',
     marginVertical: 5,
@@ -91,7 +87,7 @@ const styles = StyleSheet.create({
   button:{
     flex: 1,
     flexDirection: "column-reverse",
-    paddingBottom: 5,
+    paddingBottom: 10,
   },
   modalText:{
     color: 'white',
